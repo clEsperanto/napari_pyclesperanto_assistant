508dc2becbb83973a29b3fc95e9fd66e03aefc2f
haesleinhuepf <robert_haase@gmx.de>
2020-12-21 14:12:38 +0100

    deal with command line parameters

--- napari_pyclesperanto_assistant/__main__.py
+++ napari_pyclesperanto_assistant/__main__.py
@@ -14,11 +14,24 @@ def main():
     import pyclesperanto_prototype as cle
     from napari_pyclesperanto_assistant._gui._AssistantGui import AssistantGUI
 
-    #filename = 'data/Lund_000500_resampled-cropped.tif'
-    filename = str(Path(__file__).parent) + '/data/CalibZAPWfixed_000154_max-16.tif'
-    image = imread(filename)
-    #image = imread('https://samples.fiji.sc/blobs.png'')
+    import sys
+
+    if len(sys.argv) > 1:
+        filename = str(sys.argv[1])
+        image = imread(filename)
+    else:
+        # make some artificial cell image
+        filename = "undefined.tif"
+        labels = cle.artificial_tissue_2d(width=512, height=512, delta_x=48, delta_y=32, random_sigma_x=6, random_sigma_y=6)
+        membranes = cle.detect_label_edges(labels)
+        eroded = cle.maximum_sphere(membranes, radius_x=3, radius_y=3)
+        blurred = cle.gaussian_blur(eroded, sigma_x=3, sigma_y=3)
+        image = cle.pull_zyx(blurred)
+
+    #image = imread('https://samples.fiji.sc/blobs.png')
     #image = imread('C:/structure/data/lund_000500_resampled.tif')
+    #filename = 'data/Lund_000500_resampled-cropped.tif'
+    #filename = str(Path(__file__).parent) + '/data/CalibZAPWfixed_000154_max-16.tif'
 
     print(cle.available_device_names())
     cle.select_device("p520")
