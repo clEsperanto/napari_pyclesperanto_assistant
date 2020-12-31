dfc19324aacee81bc4359dcd1181af8a76ac6272
haesleinhuepf <robert_haase@gmx.de>
2020-12-31 21:13:24 +0100

    after attaching Gui to napari, return Gui so that we can extend it

--- napari_pyclesperanto_assistant/_gui/_napari_plugin.py
+++ napari_pyclesperanto_assistant/_gui/_napari_plugin.py
@@ -3,4 +3,6 @@ from ._AssistantGui import AssistantGUI
 def napari_plugin(viewer):
 
     # add the _gui to the viewer as a dock widget
-    viewer.window.add_dock_widget(AssistantGUI(viewer), area='right')
+    gui = AssistantGUI(viewer)
+    viewer.window.add_dock_widget(gui, area='right')
+    return gui
