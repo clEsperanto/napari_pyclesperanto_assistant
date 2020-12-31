13b4447140378ee0dbc61c6b90e591f46b3e1ced
haesleinhuepf <robert_haase@gmx.de>
2020-12-31 22:17:16 +0100

    simplified code

--- napari_pyclesperanto_assistant/_gui/_AssistantGui.py
+++ napari_pyclesperanto_assistant/_gui/_AssistantGui.py
@@ -6,7 +6,6 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QAction, QPushButton, 
 
 from .._gui._LayerDialog import LayerDialog
 from .._scriptgenerators import PythonGenerator, JythonGenerator, PythonJupyterNotebookGenerator
-from .._operations._operations import denoise, background_removal, filter, binarize, combine, label, label_processing, map, mesh, measure
 
 class AssistantGUI(QWidget):
     """This Gui takes a napari as parameter and infiltrates it.
@@ -37,16 +36,18 @@ class AssistantGUI(QWidget):
         label.setFixedSize(QSize(300, 30))
         self.layout.addWidget(label)
 
-        self._add_button("Filter (Noise removal)", self._add_denoise_clicked)
-        self._add_button("Filter (Background removal)", self._add_background_removal_clicked)
-        self._add_button("Filter", self._add_filter_clicked)
-        self._add_button("Binarize", self._add_binarize_clicked)
-        self._add_button("Combine", self._add_combine_clicked)
-        self._add_button("Label", self._add_label_clicked)
-        self._add_button("Label Processing", self._add_label_processing_clicked)
-        self._add_button("Map", self._map_clicked)
-        self._add_button("Mesh", self._mesh_clicked)
-        self._add_button("Measure", self._measure_clicked)
+        from .._operations._operations import denoise, background_removal, filter, binarize, combine, label, label_processing, map, mesh, measure
+
+        self._add_button("Filter (Noise removal)", denoise)
+        self._add_button("Filter (Background removal)", background_removal)
+        self._add_button("Filter", filter)
+        self._add_button("Binarize", binarize)
+        self._add_button("Combine", combine)
+        self._add_button("Label", label)
+        self._add_button("Label Processing", label_processing)
+        self._add_button("Map", map)
+        self._add_button("Mesh",mesh)
+        self._add_button("Measure", measure)
 
         self.layout.addStretch()
 
@@ -92,43 +93,15 @@ class AssistantGUI(QWidget):
         btn.setIconSize(QSize(20, 20))
         btn.setStyleSheet("text-align:left;")
 
+        def trigger():
+            self._activate(handler)
+
         # action
-        btn.clicked.connect(handler)
+        btn.clicked.connect(trigger)
         self.layout.addWidget(btn)
-
-    def _add_denoise_clicked(self):
-        self._activate(denoise)
-
-    def _add_background_removal_clicked(self):
-        self._activate(background_removal)
-
-    def _add_filter_clicked(self):
-        self._activate(filter)
-
-    def _add_binarize_clicked(self):
-        self._activate(binarize)
-
-    def _add_combine_clicked(self):
-        self._activate(combine)
-
-    def _add_label_clicked(self):
-        self._activate(label)
-
-    def _add_label_processing_clicked(self):
-        self._activate(label_processing)
-
-    def _measure_clicked(self):
-        self._activate(measure)
-
-    def _map_clicked(self):
-        self._activate(map)
-
-    def _mesh_clicked(self):
-        self._activate(mesh)
 
     def _activate(self, magicgui):
         LayerDialog(self.viewer, magicgui)
-
 
     def _export_python_code(self):
         generator = PythonGenerator(self.viewer.layers)
