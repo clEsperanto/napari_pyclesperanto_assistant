978636a7771d478b8f6de03e37ce7155362c45ef
haesleinhuepf <robert_haase@gmx.de>
2021-01-13 22:24:36 +0100

    store filename in layer metadata

--- docs/demo/start_assistant.py
+++ docs/demo/start_assistant.py
@@ -5,9 +5,10 @@ with napari.gui_qt():
     # start napari
     viewer = napari.Viewer()
 
-    viewer.open('../../napari_pyclesperanto_assistant/data/Lund_000500_resampled-cropped.tif')
+    filename = '../../napari_pyclesperanto_assistant/data/Lund_000500_resampled-cropped.tif'
+    layer = viewer.open(filename)
+    layer[0].metadata['filename'] = filename
 
     # attach the assistant
     import napari_pyclesperanto_assistant
     assistant_gui = napari_pyclesperanto_assistant.napari_plugin(viewer)
-
