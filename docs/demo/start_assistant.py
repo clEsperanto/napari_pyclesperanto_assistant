e49a188105b15cfad4e19eee11a7c373dd721fef
haesleinhuepf <robert_haase@gmx.de>
2021-02-13 14:10:39 +0100

    allow demo to run from any directory closes #4

--- docs/demo/start_assistant.py
+++ docs/demo/start_assistant.py
@@ -1,14 +1,19 @@
 import napari
+import napari_pyclesperanto_assistant
+from pathlib import Path
+
+root = Path(napari_pyclesperanto_assistant.__file__).parent
+filename = str(root / 'data' / 'Lund_000500_resampled-cropped.tif')
+
+
 
 # create Qt GUI context
 with napari.gui_qt():
     # start napari
     viewer = napari.Viewer()
 
-    filename = '../../napari_pyclesperanto_assistant/data/Lund_000500_resampled-cropped.tif'
     layer = viewer.open(filename)
     layer[0].metadata['filename'] = filename
 
     # attach the assistant
-    import napari_pyclesperanto_assistant
     assistant_gui = napari_pyclesperanto_assistant.napari_plugin(viewer)
