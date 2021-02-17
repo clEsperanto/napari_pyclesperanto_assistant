b32817fbe0a97d04381179e0993d411bcc0744cc
haesleinhuepf <robert_haase@gmx.de>
2021-02-17 19:21:30 +0100

    other input image

--- docs/demo/start_assistant.py
+++ docs/demo/start_assistant.py
@@ -3,9 +3,7 @@ import napari_pyclesperanto_assistant
 from pathlib import Path
 
 root = Path(napari_pyclesperanto_assistant.__file__).parent
-filename = str(root / 'data' / 'Lund_000500_resampled-cropped.tif')
-
-
+filename = str(root / 'data' / 'CalibZAPWfixed_000154_max-16.tif')
 
 # create Qt GUI context
 with napari.gui_qt():
