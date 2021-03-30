3a1d591f61d4258836d3723e645d295245611b64
haesleinhuepf <robert_haase@gmx.de>
2021-03-30 21:43:30 +0200

    update to be compatible with napari 0.4.7

--- napari_pyclesperanto_assistant/_operations/_operations.py
+++ napari_pyclesperanto_assistant/_operations/_operations.py
@@ -32,17 +32,17 @@ def _call_operation_ignoring_to_many_arguments(operation, arguments):
 def denoise(input1: Image, operation_name: str = cle.gaussian_blur.__name__, x: float = 1, y: float = 1, z: float = 0):
     if input1:
         # execute operation
-        cle_input = cle.push_zyx(input1.data)
+        cle_input = cle.push(input1.data)
         output = cle.create_like(cle_input)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input, output, x, y, z])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if denoise.call_count == 0:
+        if not hasattr(denoise.self, 'layer'):
             denoise.self.viewer.add_image(output, colormap=input1.colormap, translate=input1.translate)
         else:
             denoise.self.layer.data = output
@@ -62,17 +62,17 @@ def denoise(input1: Image, operation_name: str = cle.gaussian_blur.__name__, x: 
 def background_removal(input1: Image, operation_name: str = cle.top_hat_box.__name__, x: float = 10, y: float = 10, z: float = 0):
     if input1:
         # execute operation
-        cle_input = cle.push_zyx(input1.data)
+        cle_input = cle.push(input1.data)
         output = cle.create_like(cle_input)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input, output, x, y, z])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if background_removal.call_count == 0:
+        if not hasattr(background_removal.self, 'layer'):
             background_removal.self.viewer.add_image(output, colormap=input1.colormap, translate=input1.translate)
         else:
             background_removal.self.layer.data = output
@@ -92,17 +92,17 @@ def background_removal(input1: Image, operation_name: str = cle.top_hat_box.__na
 def filter(input1: Image, operation_name: str = cle.gamma_correction.__name__, x: float = 1, y: float = 1, z: float = 0):
     if input1:
         # execute operation
-        cle_input = cle.push_zyx(input1.data)
+        cle_input = cle.push(input1.data)
         output = cle.create_like(cle_input)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input, output, x, y, z])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if filter.call_count == 0:
+        if not hasattr(filter.self, 'layer'):
             filter.self.viewer.add_image(output, colormap=input1.colormap, translate=input1.translate)
         else:
             filter.self.layer.data = output
@@ -120,17 +120,17 @@ def filter(input1: Image, operation_name: str = cle.gamma_correction.__name__, x
     radius_y=plus_minus_1k,
     radius_z=plus_minus_1k
 )
-def binarize(input1: Image, operation_name : str = cle.threshold_otsu.__name__, radius_x : int = 1, radius_y : int = 1, radius_z : int = 0):
+def binarize(input1: Layer, operation_name : str = cle.threshold_otsu.__name__, radius_x : int = 1, radius_y : int = 1, radius_z : int = 0):
     if input1 is not None:
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
+        cle_input1 = cle.push(input1.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, output, radius_x, radius_y, radius_y])
-        output = cle.pull_zyx(output).astype(int)
+        output = cle.pull(output).astype(int)
 
         # show result in napari
-        if binarize.call_count == 0:
+        if not hasattr(binarize.self, 'layer'):
             binarize.self.viewer.add_labels(output, translate=input1.translate)
         else:
             binarize.self.layer.data = output
@@ -146,24 +146,24 @@ def binarize(input1: Image, operation_name : str = cle.threshold_otsu.__name__, 
     input2={'label':'Image 2'},
     operation_name={'label': 'Operation', 'choices':cle.operations(must_have_categories=['combine', 'in assistant']).keys()}
 )
-def combine(input1: Image, input2: Image = None, operation_name: str = cle.binary_and.__name__):
+def combine(input1: Layer, input2: Layer = None, operation_name: str = cle.binary_and.__name__):
     if input1 is not None:
         if (input2 is None):
             input2 = input1
 
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
-        cle_input2 = cle.push_zyx(input2.data)
+        cle_input1 = cle.push(input1.data)
+        cle_input2 = cle.push(input2.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, cle_input2, output])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if combine.call_count == 0:
+        if not hasattr(combine.self, 'layer'):
             combine.self.viewer.add_image(output, colormap=input1.colormap, translate=input1.translate)
         else:
             combine.self.layer.data = output
@@ -181,14 +181,14 @@ def combine(input1: Image, input2: Image = None, operation_name: str = cle.binar
 def label(input1: Layer, operation_name: str = cle.connected_components_labeling_box.__name__):
     if input1 is not None:
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
+        cle_input1 = cle.push(input1.data)
         operation = cle.operation(operation_name)
         output = cle.create_like(cle_input1)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, output])
-        output = cle.pull_zyx(output).astype(int)
+        output = cle.pull(output).astype(int)
 
         # show result in napari
-        if label.call_count == 0:
+        if not hasattr(label.self, 'layer'):
             label.self.viewer.add_labels(output, translate=input1.translate)
         else:
             label.self.layer.data = output
@@ -207,14 +207,14 @@ def label(input1: Layer, operation_name: str = cle.connected_components_labeling
 def label_processing(input1: Labels, operation_name: str = cle.exclude_labels_on_edges.__name__, min: float=0, max:float=100):
     if input1 is not None:
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
+        cle_input1 = cle.push(input1.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, output, min, max])
-        output = cle.pull_zyx(output).astype(int)
+        output = cle.pull(output).astype(int)
 
         # show result in napari
-        if label_processing.call_count == 0:
+        if not hasattr(label_processing.self, 'layer'):
             label_processing.self.viewer.add_labels(output, translate=input1.translate)
         else:
             label_processing.self.layer.data = output
@@ -235,18 +235,18 @@ def label_measurements(input1: Image, input2: Labels = None, operation_name: str
             input2 = input1
 
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
-        cle_input2 = cle.push_zyx(input2.data)
+        cle_input1 = cle.push(input1.data)
+        cle_input2 = cle.push(input2.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, cle_input2, output, n])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if label_measurements.call_count == 0:
+        if not hasattr(label_measurements.self, 'layer'):
             label_measurements.self.viewer.add_image(output, colormap='turbo', interpolation='nearest', translate=input1.translate)
         else:
             label_measurements.self.layer.data = output
@@ -263,10 +263,10 @@ def label_measurements(input1: Image, input2: Labels = None, operation_name: str
     operation_name={'label': 'Operation', 'choices':cle.operations(must_have_categories=['label measurement', 'mesh', 'in assistant'], must_not_have_categories=["combine"]).keys()},
     n = {'min': 0, 'max': 1000}
 )
-def mesh(input1: Image, operation_name : str = cle.draw_mesh_between_touching_labels.__name__, n : float = 1):
+def mesh(input1: Labels, operation_name : str = cle.draw_mesh_between_touching_labels.__name__, n : float = 1):
     if input1 is not None:
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
+        cle_input1 = cle.push(input1.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, output, n])
@@ -274,10 +274,10 @@ def mesh(input1: Image, operation_name : str = cle.draw_mesh_between_touching_la
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity - min_intensity == 0:
             max_intensity = min_intensity + 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if mesh.call_count == 0:
+        if not hasattr(mesh.self, 'layer'):
             mesh.self.viewer.add_image(output, colormap='green', blending='additive', translate=input1.translate)
         else:
             mesh.self.layer.data = output
@@ -293,20 +293,20 @@ def mesh(input1: Image, operation_name : str = cle.draw_mesh_between_touching_la
     operation_name={'label': 'Operation', 'choices':cle.operations(must_have_categories=['label measurement', 'map', 'in assistant'], must_not_have_categories=["combine"]).keys()},
     n = {'min': 0, 'max': 1000}
 )
-def map(input1: Image, operation_name: str = cle.label_pixel_count_map.__name__, n : float = 1):
+def map(input1: Labels, operation_name: str = cle.label_pixel_count_map.__name__, n : float = 1):
     if input1 is not None:
         # execute operation
-        cle_input1 = cle.push_zyx(input1.data)
+        cle_input1 = cle.push(input1.data)
         output = cle.create_like(cle_input1)
         operation = cle.operation(operation_name)
         _call_operation_ignoring_to_many_arguments(operation, [cle_input1, output, n])
         max_intensity = cle.maximum_of_all_pixels(output)
         if max_intensity == 0:
             max_intensity = 1 # prevent division by zero in vispy
-        output = cle.pull_zyx(output)
+        output = cle.pull(output)
 
         # show result in napari
-        if map.call_count == 0:
+        if not hasattr(map.self, 'layer'):
             map.self.viewer.add_image(output, colormap='turbo', interpolation='nearest', translate=input1.translate)
         else:
             map.self.layer.data = output
