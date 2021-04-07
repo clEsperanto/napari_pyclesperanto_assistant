a98bea039c49d627f221a61011da67b8a07fb589
haesleinhuepf <robert_haase@gmx.de>
2021-04-07 19:26:12 +0200

    add test using napari

--- napari_pyclesperanto_assistant/_tests/test_dock_widget.py
+++ napari_pyclesperanto_assistant/_tests/test_dock_widget.py
@@ -3,3 +3,27 @@
 # add your tests here...
 def test_whatever():
     pass
+
+def test_complex_workflow():
+    import napari
+    import napari_pyclesperanto_assistant
+    from pathlib import Path
+
+    root = Path(napari_pyclesperanto_assistant.__file__).parent
+
+    filename = str(root / 'data' / 'Lund_000500_resampled-cropped.tif')
+    # filename = str(root / 'data' / 'CalibZAPWfixed_000154_max-16.tif')
+
+    # create Qt GUI context
+    napari.gui_qt()
+    
+    # start napari
+    viewer = napari.Viewer()
+
+    layer = viewer.open(filename)
+    layer[0].metadata['filename'] = filename
+
+    # attach the assistant
+    assistant_gui = napari_pyclesperanto_assistant.napari_plugin(viewer)
+
+    viewer.close()
