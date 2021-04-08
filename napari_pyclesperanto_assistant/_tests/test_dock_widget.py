8a52b37a06453679558c9239248b52c0e395a4cf
haesleinhuepf <rhaase@mpi-cbg.de>
2021-04-09 00:07:02 +0200

    wip: more detailed tests

--- napari_pyclesperanto_assistant/_tests/test_dock_widget.py
+++ napari_pyclesperanto_assistant/_tests/test_dock_widget.py
@@ -3,12 +3,27 @@
 def test_whatever():
     pass
 
+from pathlib import Path
+import napari_pyclesperanto_assistant
+from .._operations._operations import denoise, background_removal, filter, binarize, combine, label, \
+    label_processing, map, mesh, measure, label_measurements, transform, projection
+
 
 def test_whatever2(make_napari_viewer):
 
     viewer = make_napari_viewer()
 
-#    from .._operations._operations import denoise
+    assistant_gui = napari_pyclesperanto_assistant.napari_plugin(viewer)
+
+    root = Path(napari_pyclesperanto_assistant.__file__).parent
+    filename = str(root / 'data' / 'CalibZAPWfixed_000154_max-16.tif')
+    layer = viewer.open(filename)
+
+    #assistant_gui._activate(denoise)
+    #assistant_gui._activate(background_removal)
+    #assistant_gui._activate(filter)
+    #assistant_gui._activate(binarize)
+
 
 #def test_whatever3(make_napari_viewer):
 #
