95fcfb4d54a2388338030597fa9534cc4baa85fa
haesleinhuepf <robert_haase@gmx.de>
2021-04-11 22:42:33 +0200

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(filename) as f:
 use_scm = {"write_to": "napari_pyclesperanto_assistant/_version.py"}
 setup(
     name="napari_pyclesperanto_assistant",
-    version="0.7.5",
+    version="0.7.6",
     author="Robert Haase",
     author_email="robert.haase@tu-dresden.de",
     description="OpenCL based GPU-accelerated image processing in napari",
