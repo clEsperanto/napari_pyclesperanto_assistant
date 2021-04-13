46c8d24f24c3af5ca003237349630961748c45fa
Talley Lambert <talley.lambert@gmail.com>
2021-04-13 17:11:58 -0400

    work with 0.4.8

--- napari_pyclesperanto_assistant/_gui/_Assistant.py
+++ napari_pyclesperanto_assistant/_gui/_Assistant.py
@@ -12,11 +12,18 @@ from .._pipeline import Pipeline
 from ._button_grid import ButtonGrid
 from ._category_widget import OP_ID, OP_NAME_PARAM, VIEWER_PARAM, make_gui_for_category
 
+
 if TYPE_CHECKING:
     from magicgui.widgets import FunctionGui
     from napari._qt.widgets.qt_viewer_dock_widget import QtViewerDockWidget
     from napari.layers import Layer
     from napari.viewer import Viewer
+
+from napari import __version__ as napari_version
+from packaging.version import parse as parse_version
+
+npv = parse_version(napari_version)
+NAP048 = (npv.major, npv.minor, npv.micro) >= (0, 4, 8)
 
 
 class Assistant(QWidget):
@@ -37,9 +44,12 @@ class Assistant(QWidget):
     def __init__(self, napari_viewer: Viewer):
 
         super().__init__(napari_viewer.window.qt_viewer)
-        self.viewer = napari_viewer
+        self._viewer = napari_viewer
         napari_viewer.layers.events.removed.connect(self._on_layer_removed)
-        napari_viewer.events.active_layer.connect(self._on_active_layer_change)
+        if NAP048:
+            napari_viewer.layers.selection.events.changed.connect(self._on_selection)
+        else:
+            napari_viewer.events.active_layer.connect(self._on_active_layer_change)
         self._layers: Dict[Layer, Tuple[QtViewerDockWidget, FunctionGui]] = {}
 
         icon_grid = ButtonGrid(self)
@@ -61,6 +71,13 @@ class Assistant(QWidget):
         self.layout().addWidget(icon_grid)
         self.layout().addLayout(export_btns)
 
+    def _on_selection(self, event):
+        for layer, (dw, gui) in self._layers.items():
+            if layer in self._viewer.layers.selection:
+                dw.show()
+            else:
+                dw.hide()
+
     def _on_active_layer_change(self, event):
         for layer, (dw, gui) in self._layers.items():
             dw.show() if event.value is layer else dw.hide()
@@ -69,17 +86,25 @@ class Assistant(QWidget):
         layer = event.value
         if layer in self._layers:
             dw = self._layers[layer][0]
-            self.viewer.window.remove_dock_widget(dw)
+            self._viewer.window.remove_dock_widget(dw)
             # remove layer from internal list
             self._layers.pop(layer)
 
     def _on_item_clicked(self, item):
         self._activate(CATEGORIES.get(item.text()))
 
+    def _get_active_layer(self):
+        if NAP048:
+            return self._viewer.layers.selection.active
+        else:
+            return self._viewer.active_layer
+
     def _activate(self, category: Category):
-        if not self.viewer.active_layer:
+        # get currently active layer (before adding dock widget)
+        input_layer = self._get_active_layer()
+        if not input_layer:
             warn("Please select a layer first")
-            return
+            return False
 
         # make a new widget
         gui = make_gui_for_category(category)
@@ -87,10 +112,8 @@ class Assistant(QWidget):
         # do this here rather than widget creation for the sake of
         # non-Assistant-based widgets.
         gui._auto_call = False
-        # get currently active layer (before adding dock widget)
-        input_layer = self.viewer.active_layer
         # add gui to the viewer
-        dw = self.viewer.window.add_dock_widget(gui, area="right", name=category.name)
+        dw = self._viewer.window.add_dock_widget(gui, area="right", name=category.name)
         # make sure the originally active layer is the input
         gui.input0.value = input_layer
         # call the function widget &
@@ -103,7 +126,7 @@ class Assistant(QWidget):
 
     def load_sample_data(self, fname="Lund_000500_resampled-cropped.tif"):
         data_dir = Path(__file__).parent.parent / "data"
-        self.viewer.open(str(data_dir / fname))
+        self._viewer.open(str(data_dir / fname))
 
     def to_dask(self):
         graph = {}
