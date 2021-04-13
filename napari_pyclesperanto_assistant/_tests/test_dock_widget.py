638728fddfa6a5eabf088cac962b4d7ad6b2bc95
Talley Lambert <talley.lambert@gmail.com>
2021-04-13 16:53:16 -0400

    update tests

--- napari_pyclesperanto_assistant/_tests/test_dock_widget.py
+++ napari_pyclesperanto_assistant/_tests/test_dock_widget.py
@@ -1,31 +1,18 @@
 from napari import Viewer
-from napari_pyclesperanto_assistant._gui import Assistant
+from napari_pyclesperanto_assistant._categories import CATEGORIES
+import pytest
 
 
-def test_complex_workflow(qtbot, tmp_path):
-
+@pytest.fixture
+def assistant(qtbot):
     viewer = Viewer(show=False)
-    viewer.window.add_plugin_dock_widget("clEsperanto")
+    viewer.window.add_plugin_dock_widget("clEsperanto", "Assistant")
     dw = viewer.window._dock_widgets["clEsperanto: Assistant"]
-    assistant_gui: Assistant = dw.widget()
-    assistant_gui.load_sample_data()
+    return dw.widget()
 
-    assistant_gui._activate("denoise")
-    assistant_gui._activate("background_removal")
-    assistant_gui._activate("filter")
-    assistant_gui._activate("binarize")
-    assistant_gui._activate("label")
-    assistant_gui._activate("label_processing")
-    assistant_gui._activate("map")
-    assistant_gui._activate("combine")
 
-    # assistant_gui._activate('label')
-    # assistant_gui._activate('mesh')
-    # assistant_gui._activate('measure')
-    # assistant_gui._activate('label_measurements')
-
-    assistant_gui._activate("transform")
-    assistant_gui._activate("projection")
-
-    assistant_gui._export_jython_code_to_clipboard()
-    # assistant_gui._export_notebook(filename='test.ipynb')
+@pytest.mark.parametrize("category", CATEGORIES.values(), ids=lambda c: c.name)
+def test_individual_categories(category, assistant):
+    assistant.load_sample_data()
+    assistant._activate(category)
+    assistant.to_clipboard()
