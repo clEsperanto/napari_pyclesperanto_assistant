db0607a93e6bf59a6678e6ca3f76bf233636e009
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-07-11 12:22:01 +0200

    bugfix: some operations were not executed

--- napari_pyclesperanto_assistant/_convert_to_numpy.py
+++ napari_pyclesperanto_assistant/_convert_to_numpy.py
@@ -9,27 +9,21 @@ from napari.layers import Image, Labels, Layer
 LayerInput = Annotated[Layer, {"label": "Image"}]
 
 
-def convert_to_numpy(layer : LayerInput, napari_viewer : Viewer) -> Layer:
-    napari_viewer.window.remove_dock_widget(convert_to_numpy.native)
-
+def convert_to_numpy(layer : LayerInput) -> Layer:
     if isinstance(layer, Labels):
         return Labels(np.asarray(layer.data), name="np " + layer.name)
     else:
         return Image(np.asarray(layer.data), name="np " + layer.name)
 
 
-def convert_to_2d_timelapse(layer : LayerInput, napari_viewer : Viewer) -> Layer:
-    napari_viewer.window.remove_dock_widget(convert_to_2d_timelapse.native)
-
+def convert_to_2d_timelapse(layer : LayerInput) -> Layer:
     if isinstance(layer, Labels):
         return Labels(np.expand_dims(layer.data, axis=1), name="2d+t " + layer.name)
     else:
         return Image(np.expand_dims(layer.data, axis=1), name="2d+t " + layer.name)
 
 
-def make_labels_editable(labels : Labels, napari_viewer : Viewer) -> Labels:
-    napari_viewer.window.remove_dock_widget(make_labels_editable.native)
-
+def make_labels_editable(labels : Labels) -> Labels:
     return Labels(np.asarray(labels.data), name="np " + labels.name)
 
 
