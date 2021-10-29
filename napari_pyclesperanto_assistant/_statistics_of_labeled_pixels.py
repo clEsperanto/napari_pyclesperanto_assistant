abae4d0266aaf5d06fda595116547bc2199b71a7
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-10-29 21:36:28 +0200

    added measurements to menu, added interactivity to results table

--- napari_pyclesperanto_assistant/_statistics_of_labeled_pixels.py
+++ napari_pyclesperanto_assistant/_statistics_of_labeled_pixels.py
@@ -6,13 +6,20 @@ from napari_plugin_engine import napari_hook_implementation
 from napari.types import ImageData, LabelsData, LayerDataTuple
 from napari import Viewer
 from pandas import DataFrame
+from qtpy.QtCore import QTimer
 from qtpy.QtWidgets import QTableWidget, QTableWidgetItem, QWidget, QGridLayout, QPushButton, QFileDialog
 import pyclesperanto_prototype as cle
+import napari
 
-def statistics_of_labeled_pixels(image: ImageData, labels: LabelsData, napari_viewer : Viewer, measure_background=False):
+from napari_tools_menu import register_function
+
+@register_function(menu="Measurement > Statistics of labeled pixels (clEsperanto)")
+def statistics_of_labeled_pixels(image: ImageData, labels_layer: napari.layers.Labels, napari_viewer : Viewer, measure_background=False):
     """
     Adds a table widget to a given napari viewer with quantitative analysis results derived from an image-labelimage pair.
     """
+    labels = labels_layer.data
+
     if image is not None and labels is not None:
 
         # quantitative analysis using clEsperanto's statistics_of_labelled_pixels
@@ -21,34 +28,64 @@ def statistics_of_labeled_pixels(image: ImageData, labels: LabelsData, napari_vi
         else:
             table = cle.statistics_of_labelled_pixels(image, labels)
 
+        # Store results in the properties dictionary:
+        labels_layer.properties = table
+
         # turn table into a widget
-        dock_widget = table_to_widget(table)
+        dock_widget = table_to_widget(table, labels_layer)
 
         # add widget to napari
         napari_viewer.window.add_dock_widget(dock_widget, area='right')
     else:
         warnings.warn("Image and labels must be set.")
 
-def table_to_widget(table: dict) -> QWidget:
+# taken from https://github.com/haesleinhuepf/napari-skimage-regionprops/blob/master/napari_skimage_regionprops/_regionprops.py#L90
+def table_to_widget(table: dict, labels_layer: napari.layers.Labels) -> QWidget:
     """
     Takes a table given as dictionary with strings as keys and numeric arrays as values and returns a QWidget which
     contains a QTableWidget with that data.
     """
     view = Table(value=table)
 
+    if labels_layer is not None:
+
+        @view.native.clicked.connect
+        def clicked_table():
+            row = view.native.currentRow()
+            label = table["label"][row]
+            labels_layer.selected_label = label
+
+        def after_labels_clicked():
+            row = view.native.currentRow()
+            label = table["label"][row]
+            if label != labels_layer.selected_label:
+                for r, l in enumerate(table["label"]):
+                    if l ==  labels_layer.selected_label:
+                        view.native.setCurrentCell(r, view.native.currentColumn())
+                        break
+
+        @labels_layer.mouse_drag_callbacks.append
+        def clicked_labels(event, event1):
+            # We need to run this lagter as the labels_layer.selected_label isn't changed yet.
+            QTimer.singleShot(200, after_labels_clicked)
+
+
+
     copy_button = QPushButton("Copy to clipboard")
+
     @copy_button.clicked.connect
     def copy_trigger():
         view.to_dataframe().to_clipboard()
 
     save_button = QPushButton("Save as csv...")
+
     @save_button.clicked.connect
     def save_trigger():
         filename, _ = QFileDialog.getSaveFileName(save_button, "Save as csv...", ".", "*.csv")
         view.to_dataframe().to_csv(filename)
 
     widget = QWidget()
-    widget.setWindowTitle("Statistics of labeled pixels")
+    widget.setWindowTitle("region properties")
     widget.setLayout(QGridLayout())
     widget.layout().addWidget(copy_button)
     widget.layout().addWidget(save_button)
