09268474686fcc1254c42541b2b46930dd58c90c
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-10-30 15:14:51 +0200

    load example data from online, added simulated data

--- napari_pyclesperanto_assistant/_napari_plugin.py
+++ napari_pyclesperanto_assistant/_napari_plugin.py
@@ -13,6 +13,7 @@ from ._convert_to_numpy import convert_to_numpy, convert_to_2d_timelapse, make_l
     reset_brightness_contrast, auto_brightness_contrast, split_stack, auto_brightness_contrast_all_images, \
     set_voxel_size, set_voxel_size_of_all_layers, convert_labels_to_image, convert_image_to_labels
 from ._categories import attach_tooltips
+from skimage.io import imread
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
@@ -22,15 +23,45 @@ def napari_experimental_provide_dock_widget():
         for name, category in CATEGORIES.items()
     ]
 
+def _load_Lund():
+    return [(imread("https://github.com/clEsperanto/clesperanto_example_data/raw/main/Lund_000500_resampled-cropped.tif"), {'name': 'Lund'})]
+
+def _load_CalibZAPWfixed():
+    return [(imread("https://github.com/clEsperanto/clesperanto_example_data/raw/main/CalibZAPWfixed_000154_max.tif"), {'name': 'CalibZAPWfixed'})]
+
+def _load_Sonneberg():
+    return [(imread("https://github.com/clEsperanto/clesperanto_example_data/raw/main/Sonneberg100_Resampled_RotY-40_MaxZ.tif"), {'name': 'Sonneberg'})]
+
+def _load_Haase_MRT_tfl3d1():
+    return [(imread("https://github.com/clEsperanto/clesperanto_example_data/raw/main/Haase_MRT_tfl3d1.tif"), {'name': 'Haase MRT'})]
+
+def _load_Pixel_cat():
+    return [(imread("https://github.com/clEsperanto/clesperanto_example_data/raw/main/pixel_cat.tif"), {'name': 'Pixel cat'})]
+
+def _load_perfect_tissue():
+    import pyclesperanto_prototype as cle
+    return [(cle.artificial_tissue_2d(random_sigma_x=1, random_sigma_y=0), {'name': 'Artifical perfect tissue (pos sigma 0)'})]
+
+def _load_orderly_tissue():
+    import pyclesperanto_prototype as cle
+    return [(cle.artificial_tissue_2d(random_sigma_x=1, random_sigma_y=2), {'name': 'Artifical orderly tissue (pos sigma 2)'})]
+
+def _load_chaotic_tissue():
+    import pyclesperanto_prototype as cle
+    return [(cle.artificial_tissue_2d(random_sigma_x=1, random_sigma_y=8), {'name': 'Artifical chaotic tissue (pos sigma 8)'})]
 
 @napari_hook_implementation
 def napari_provide_sample_data():
     data = Path(__file__).parent / "data"
     return {
-        "Lund": data / "Lund_000500_resampled-cropped.tif",
-        "CalibZAPWfixed": data / "CalibZAPWfixed_000154_max.tif",
-        "Sonneberg": data / "Sonneberg100_Resampled_RotY-40_MaxZ.tif",
-        "Haase_MRT_tfl3d1": data / "Haase_MRT_tfl3d1.tif",
+        "Lund": _load_Lund,
+        "CalibZAPWfixed": _load_CalibZAPWfixed,
+        "Sonneberg": _load_Sonneberg,
+        "Haase_MRT_tfl3d1": _load_Haase_MRT_tfl3d1,
+        "Pixel (cat)": _load_Pixel_cat,
+        "Artificial perfect tissue": _load_perfect_tissue,
+        "Artificial orderly tissue": _load_orderly_tissue,
+        "Artificial chaotic tissue": _load_chaotic_tissue,
         "Blobs (from ImageJ)": data / "blobs.tif",
     }
 
