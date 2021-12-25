2ef48f5022c3b56c2feff787623b8daa4cb538cb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-25 22:12:09 +0100

    extend test with a workflow

--- napari_pyclesperanto_assistant/_tests/test_dock_widget.py
+++ napari_pyclesperanto_assistant/_tests/test_dock_widget.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from napari import Viewer
 from napari_pyclesperanto_assistant._categories import CATEGORIES, LabelsInput
@@ -55,3 +56,23 @@ def test_something_with_viewer(make_napari_viewer):
     num_dw = len(viewer.window._dock_widgets)
     viewer.window.add_dock_widget(assistant)
     assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    image = np.asarray([
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 1, 1],
+        [1, 2, 1, 0, 0, 1, 2],
+        [1, 1, 1, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 2, 1, 0, 0],
+    ])
+
+    viewer.add_image(image)
+
+    assistant._activate(CATEGORIES.get("Remove noise"))
+    assistant._activate(CATEGORIES.get("Remove background"))
+    assistant._activate(CATEGORIES.get("Filter"))
+    assistant._activate(CATEGORIES.get("Binarize"))
+    assistant._activate(CATEGORIES.get("Label"))
+    assistant._activate(CATEGORIES.get("Measure labeled image"))
+    assistant.seach_field.setText("Gauss")
