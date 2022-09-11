44aee43bafb87bfe1f1a9c453c712f282d0f3a49
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-09-11 18:49:54 +0200

    add smooth_labels and merge_labels_with_border_intensity_within_range

--- napari_pyclesperanto_assistant/_napari_cle_functions.py
+++ napari_pyclesperanto_assistant/_napari_cle_functions.py
@@ -16,3 +16,21 @@ def label(binary_image: napari.types.LabelsData, viewer: napari.Viewer = None) -
 def voronoi_otsu_labeling(image:ImageData, spot_sigma: float = 2, outline_sigma: float = 2, viewer: napari.Viewer = None) -> LabelsData:
     return cle.voronoi_otsu_labeling(image, spot_sigma=spot_sigma, outline_sigma=outline_sigma)
 
+
+@register_function(menu="Segmentation post-processing > Smooth labels (clesperanto)", radius={"widget_type": "FloatSlider", "min": 0, "max":10}, auto_call=True)
+@time_slicer
+def smooth_labels(labels:LabelsData, radius: float = 1) -> LabelsData:
+    return cle.smooth_labels(labels, radius=radius)
+
+
+@register_function(menu="Segmentation post-processing > Merge labels with intensity along borders within range (clesperanto)",
+                   minimum_intensity={"min": 0, "max":100000},
+                   maximum_intensity={"min": 0, "max":100000},
+                   auto_call=True)
+@time_slicer
+def merge_labels_with_border_intensity_within_range(image:ImageData, labels:LabelsData, minimum_intensity: float = 0, maximum_intensity: float = 1000) -> LabelsData:
+    return cle.merge_labels_with_border_intensity_within_range(image,
+                                                               labels,
+                                                               minimum_intensity=minimum_intensity,
+                                                               maximum_intensity=maximum_intensity
+                                                               )
