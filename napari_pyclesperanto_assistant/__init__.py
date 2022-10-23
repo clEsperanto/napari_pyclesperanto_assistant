7e0df1fb429cf1aaa3f3a44dbff9e6e8e01174c0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-10-23 09:49:56 +0200

    make cle-functions available via API

--- napari_pyclesperanto_assistant/__init__.py
+++ napari_pyclesperanto_assistant/__init__.py
@@ -1,5 +1,6 @@
 from ._gui import Assistant
 from ._convert_to_numpy import *
-from ._napari_cle_functions import label, voronoi_otsu_labeling
+from ._napari_cle_functions import *
 
 __version__ = "0.20.0"
+__common_alias__ = "ncle"
